8f39950b3476b117c0acf16f4818f4ed360b7786
MW-Resurgent <mwu2021@gmail.com>
2019-07-29 15:31:39 -0700

    Rebuilt Project, laid skeleton draft of calculator params.
    - Rebuilt the project from the ground up (deleted fx-roi folder), reconfigured entire Feact project.
    
    - Imported dependencies (such as customize-cra, less/less-loader, react-app-rewired, babel-plugin-import, ant-d) to configure new project exactly as listed in the Ant-Design tutorial:  https://ant.design/docs/react/use-with-create-react-app.
    
    - Entire Application is simply rendered from App.js right now.  Parameters are all in <h> HTML title names.  Will be replaced with appropriate elements later.  Just drafting parameters required now.
    
    See 07.29.2019 entry in project journal for more information.

--- fx-roi/src/App.js
+++ fx-roi/src/App.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-
 import {
+  Button,
   Layout,
   Row,
   Col
@@ -10,33 +9,53 @@ import {
 
 const { Header, Footer, Sider, Content } = Layout;
 
-function App() {
-  return (
-    <div className="App">
-      <Layout>
-        <Header>
-          <Row>
-            <Col>
-              test
-            </Col>
-            <Col>
-              test
-            </Col>
-            <Col>
-              test
-            </Col>
-          </Row>
-        </Header>
-        <Content>
 
-        </Content>
-        <Footer>
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Layout>
+          <Header>
+            <Row>
+            <h1>Annual Costs Calculator</h1>
+              <Col>
+                <h1>Enter Your Costs</h1>
+                <h3>Number of APIs You Have</h3>
+                <h3>Endpoints Per API</h3>
+                <h3>Unique Security Validations Per API</h3>
+                
+              </Col>
+              <Col>
+                <h1>Comparison of Programs</h1>
+                <h2>APISec™</h2>
+                <h3>Select an FX Lab Product Plan:</h3>
+                <h2>Bounty Program</h2>
+                <h3>Average Payout Per Bug Found</h3>
+                <h2>Breach Cost</h2>
+                <h3>Cost of Fines</h3>
+                <h3>Breach Probability (annually per API)</h3>
+                <h2>In-house Cost</h2>
+                <h3>Development Time Per Validation</h3>
+                <h3>Security Engineers on Team</h3>
+                <h3>Hourly Wage Per Engineer</h3>
+              </Col>
+              <Col>
+                test
+              </Col>
+            </Row>
+          </Header>
+          <Content>
 
-        </Footer>
-      </Layout>
+          </Content>
+          <Footer>
+
+          </Footer>
+        </Layout>
       
-    </div>
-  );
+        <Button type="primary">Button</Button>
+      </div>
+    );
+  }
 }
 
 export default App;
